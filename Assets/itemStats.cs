12dec0cb9a1df007ab97a101bffb56e404ebfacd
danteholm <danteholm@hotmail.com>
2015-02-07 02:36:28 +0100

    with mouseOver and itemStats scripts

---
+++ Assets/itemStats.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class itemStats : MonoBehaviour {
+	public string objectName,objectFlair;
+	public short goldValue, Bonus1, Bonus2;
+	public sbyte type = 2;
+
+	public short armorBonus, attackBonus,hungerBonus,hitsLeft;
+	public string objectEffect1,objectEffect2, typeList;
+	public enum AttackType {Fire, Sharpness , Cold, Acid, Lightning};
+	public enum DefenceType {Fire, Sharpness , Cold, Acid, Lightning};
+	
+
+	// Use this for initialization
+	void Start () {
+
+
+		if (type == 1){
+			typeList = "Armor";
+			armorBonus = 10;
+			hitsLeft = 5;
+			objectEffect1 ="+"+Bonus1+" "+DefenceType.Fire+" resistance";
+			objectEffect2 ="+"+Bonus2+" "+DefenceType.Sharpness+" resistance";
+			
+			
+		}
+		if (type == 2){
+			typeList = "Weapon";
+			attackBonus = 5;
+			hitsLeft = 3;
+			objectEffect1 ="+"+Bonus1+" "+AttackType.Sharpness+" damage";
+			objectEffect2 ="+"+Bonus2+" "+AttackType.Fire+" damage";
+			
+		}
+		if (type == 3){
+			typeList = "Food";
+			hungerBonus = 30;
+			
+		}
+		if (type == 4){
+			typeList = "Valuable";
+
+			
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+	}
+}
---
+++ Assets/mouseOver.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class mouseOver : MonoBehaviour {
+
+	public string targetItem;
+	private string objectEffect1;
+	private string objectEffect2;
+	private string objectFlair;
+
+	private bool _displayObjectName = false;
+	// Use this for initialization
+	void OnGUI(){
+		DisplayName();
+	}
+
+	void OnMouseEnter(){
+		_displayObjectName = true;
+	}
+	void OnMouseExit(){
+		_displayObjectName = false;
+	}
+	public void DisplayName(){
+		if (_displayObjectName == true) {
+			GUI.Box(new Rect(Event.current.mousePosition.x-155,Event.current.mousePosition.y,150,128),"Name: "+GameObject.Find(targetItem).GetComponent<itemStats>().objectName+  
+			        																				  "\n Type: " 
+			        																				  +GameObject.Find(targetItem).GetComponent<itemStats>().typeList+
+			        																				  "\n Gold Value: " 
+			       																					  +GameObject.Find(targetItem).GetComponent<itemStats>().goldValue+
+			        																				  "\n \n"
+			        																				  +GameObject.Find(targetItem).GetComponent<itemStats>().objectEffect1+ 
+			        																				  "\n" 
+			        																				  +GameObject.Find(targetItem).GetComponent<itemStats>().objectEffect2+ 
+			        																				  "\n \n" 
+			        																				  +GameObject.Find(targetItem).GetComponent<itemStats>().objectFlair);
+		}
+	}
+}
