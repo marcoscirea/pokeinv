a68ee947fadccce604b1519b1a9017130c7b1e31
marcoscirea <marco.prolog@gmail.com>
2015-02-08 03:24:05 +0100

    New texture for merchant + icons

--- Assets/Scripts/EquipVisualization.cs
+++ Assets/Scripts/EquipVisualization.cs
@@ -11,11 +11,20 @@ public class EquipVisualization : MonoBehaviour {
 	public Sprite sword;
 	public Sprite wood;
 
-	public Sprite Default;
+	public Sprite defaultHat;
+	public Sprite defaultChest;
+	public Sprite defaultPants;
+	public Sprite defaultBoots;
+	public Sprite defaultWeapon;
 
 	// Use this for initialization
 	void Start () {
-		Default = transform.FindChild("Hat").GetComponent<Image>().sprite;
+		//Default = transform.FindChild("Hat").GetComponent<Image>().sprite;
+		defaultHat = transform.FindChild("Hat").GetComponent<Image>().sprite;
+		defaultChest = transform.FindChild("Chest").GetComponent<Image>().sprite;
+		defaultPants = transform.FindChild("Pants").GetComponent<Image>().sprite;
+		defaultBoots = transform.FindChild("Boots").GetComponent<Image>().sprite;
+		defaultWeapon = transform.FindChild("Weapon").GetComponent<Image>().sprite;
 	}
 	
 	// Update is called once per frame
@@ -47,22 +56,22 @@ public class EquipVisualization : MonoBehaviour {
 	}
 
 	public void RemoveHat(){
-		transform.FindChild("Hat").GetComponent<Image>().sprite = Default;
+		transform.FindChild("Hat").GetComponent<Image>().sprite = defaultHat;
 	}
 
 	public void RemoveChest(){
-		transform.FindChild("Chest").GetComponent<Image>().sprite = Default;
+		transform.FindChild("Chest").GetComponent<Image>().sprite = defaultChest;
 	}
 
 	public void RemovePants(){
-		transform.FindChild("Pants").GetComponent<Image>().sprite = Default;
+		transform.FindChild("Pants").GetComponent<Image>().sprite = defaultPants;
 	}
 
 	public void RemoveBoots(){
-		transform.FindChild("Boots").GetComponent<Image>().sprite = Default;
+		transform.FindChild("Boots").GetComponent<Image>().sprite = defaultBoots;
 	}
 
 	public void RemoveWeapon(){
-		transform.FindChild("Weapon").GetComponent<Image>().sprite = Default;
+		transform.FindChild("Weapon").GetComponent<Image>().sprite = defaultWeapon;
 	}
 }
